8a7c2a2ca2afab1f27d8d479c9f66feb522c25de
prycedom <prycedom@gmail.com>
2020-05-06 18:50:25 -0400

    Corrected html and script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 <script>
     lightbox.option({
       'alwaysShowNavOnTouchDevices':true
-      'albumLabel': true
     })
 </script>
+document.getElementById("searchhere").addEventListener("keyup", myFunction);
+  function myFunction() {
+    var x = document.getElementById("searchhere");
+    x.value = x.value.toLowerCase();
+    console.log(x.value);
+  }
 <script>
   $(document).ready(function(){
     $("#sticker").sticky({topSpacing:0});
