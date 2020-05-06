53c2946fbdabae974aacc5ea1c14570182154fac
prycedom <prycedom@gmail.com>
2020-05-06 19:16:58 -0400

    Corrected script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-<script>
+
     lightbox.option({
       'alwaysShowNavOnTouchDevices':true
     })
-</script>
 document.getElementById("searchhere").addEventListener("keyup", myFunction);
   function myFunction() {
     var x = document.getElementById("searchhere");
     x.value = x.value.toLowerCase();
     console.log(x.value);
   }
-<script>
-  $(document).ready(function(){
-    $("#sticker").sticky({topSpacing:0});
-  });
-</script>
